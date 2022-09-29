cd9ccb8ca5bace09a0aadbbfc2a407eb2e620112
robertocajueiro <beto.cajueiro@gmail.com>
2022-09-29 15:28:43 -0300

    first commit

---
+++ src/test/java/com/roberto/helpdesk/HelpedeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpedeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
